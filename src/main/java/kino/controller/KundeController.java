f93015c01517756223511ee0688b87a2b1daec14
sarinang92 <101523586+sarinang92@users.noreply.github.com>
2025-05-21 22:03:24 +0200

    Update KundeController.java

--- src/main/java/kino/controller/KundeController.java
+++ src/main/java/kino/controller/KundeController.java
@@ -7,21 +7,28 @@ import kino.view.KundeView;
 import java.util.List;
 
 public class KundeController {
+    //Oppretter service- og view-objekter
     private final KundeService service = new KundeService();
     private final KundeView view = new KundeView();
 
+    //kundemeny: velge visning, velge plasser og bekrefte bestilling.
     public void start() {
+        //Hent alle visninger med tilgjengelighetsinfo, sortert på filmnavn
         List<VisningMedPlassinfo> visninger = service.hentVisningerMedPlassinfo(KundeService.Sortering.FILMNAVN);
+        // velge en visning
         Visning valgt = view.visValgAvVisning(visninger);
 
         if (valgt == null) {
             System.out.println("Tilbake til hovedmeny.");
             return;
         }
-
+//hvilken visning som er valgt
         service.velgVisning(valgt);
 
+//Hent ledige plasser for valgt visning
         List<Plass> ledige = service.hentLedigePlasser();
+
+        //velge en eller flere plasser
         List<Plass> valgte = view.velgPlasser(ledige);
 
         if (valgte.isEmpty()) {
@@ -32,7 +39,7 @@ public class KundeController {
         for (Plass p : valgte) {
             service.togglePlass(p);
         }
-
+        //Vis oppsummering og spør om brukeren vil bekrefte
         service.visBekreftelse();
         int bekreft = view.hentBrukerValg();
         if (bekreft == 1) {
@@ -42,3 +49,4 @@ public class KundeController {
         }
     }
 }
+
